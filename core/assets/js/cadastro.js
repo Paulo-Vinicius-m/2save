319680720ef61d41a0c7b04e0f20a2cdb70d3542
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-01-02 22:15:27 -0300

    feat: criação da pasta core
    foi criado a pasta core, com as pastas para o js, css e img

---
+++ core/assets/js/cadastro.js
@@ -0,0 +1,50 @@
+
+async function cadastro(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const email2 = document.getElementById("confirmacao-email").value.trim();
+    const password = document.getElementById("senha").value;
+    const identificador = document.getElementById("cpf").value.trim();
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if (email !== email2) {
+        alert("Os emails não conferem.");
+        return;
+    }
+
+    const userData = { 
+        name,
+        email, 
+        email2, 
+        password, 
+        tipo: tipo.value, 
+        identificador, 
+    };
+
+    try {
+        const response = await fetch("/api/users/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userData)
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            alert("Cadastro realizado com sucesso!");
+            localStorage.setItem("authToken", result.token);
+            window.location.href = "/login"; // Redireciona para a tela de login
+        } else {
+            alert(`Erro no cadastro: ${result.message || "Erro desconhecido."}`);
+        }
+
+    } catch (error) {
+        console.error("Erro no cadastro:", error);
+        alert("Erro ao tentar realizar o cadastro. Tente novamente mais tarde.");
+    } 
+}
+
+let formularioCadastro = document.getElementById('btnCadastro');
+formularioCadastro.addEventListener('submit', cadastro);
