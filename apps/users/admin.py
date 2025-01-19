187a3e729f3bc83b4ca0109f459d2cfd2ab85c52
Paulo Vinícius <pauloviniciusmc5@gmail.com>
2025-01-19 17:28:03 -0300

    FIX admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Restaurante, Consumidor, Produto
+from .models import Restaurante, Consumidor, Produto, Carrinho, Pedido
 
 
 # Register your models here.
 admin.site.register(Restaurante)
 admin.site.register(Consumidor)
+admin.site.register(Produto)
+admin.site.register(Carrinho)
+admin.site.register(Pedido)
