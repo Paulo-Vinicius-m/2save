4e59ddc1ddb284075987fa42d081e2842ba09f1c
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-01-22 17:22:59 -0300

    ajustes na tela de pedido

--- core/assets/js/pedidos.js
+++ core/assets/js/pedidos.js
@@ -38,3 +38,30 @@ function obterPedidos() {
 
 // Chama a função para obter os pedidos ao carregar a página
 document.addEventListener('DOMContentLoaded', obterPedidos());
+
+// Menu Hamburguer
+const navbarToggle = document.querySelector('.navbar-toggler');
+    const menu = document.getElementById('navbarMenu');
+    
+    navbarToggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const response = await fetch('/assets/js/perfil-restaurante.json');
+      const userInfo = await response.json();
+      
+      // Atualizando os elementos no DOM
+      document.getElementById("user-image").src = userInfo.imagem;
+      document.getElementById("user-name").textContent = userInfo.nome;
+      document.getElementById("user-email").textContent = userInfo.email;
+      document.getElementById("user-cpf").textContent = userInfo.cpf;
+      document.getElementById("user-company").textContent = userInfo.empresa;
+      
+      
+    }catch (error) {
+        console.error('Erro ao carregar dados:', error);
+    }
+ 
+});
